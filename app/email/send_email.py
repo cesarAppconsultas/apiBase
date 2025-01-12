279bf9fbe97cd012240561ad673d9770b05ad14d
cesar angola <cesarmilan152@gmail.com>
2025-01-12 14:57:01 -0300

    update-email

--- app/email/send_email.py
+++ app/email/send_email.py
@@ -4,27 +4,41 @@ from flask_mail import Mail, Message
 from app import app
 
 from app.utils.send_email import mail
+from datetime import datetime
+
 
 @app.route('/email/send_email', methods=['POST'])
 def send_email():
     if request.method == 'POST':
         recipient = request.json.get('recipient')
+        sender_name = request.json.get('sender_name')  # Nombre del usuario que envía
+        sender_phone = request.json.get('sender_phone')  # Teléfono del usuario que envía
         subject = request.json.get('subject')
         message = request.json.get('message')
-        additional_text = request.json.get('additional_text')  # New field for additional text
+        additional_text = request.json.get('additional_text')
 
-        if not recipient or not subject or not message:
-            return jsonify({'error': 'Missing parameters'}), 400
+        # Validar que los datos obligatorios estén presentes
+        if not recipient or not sender_name or not sender_phone:
+            return jsonify({'error': 'Missing required parameters: recipient, sender_name, or sender_phone'}), 400
 
-        # Combine the message and additional text
-        complete_message = f'{message}\n\nAdditional Text: {additional_text}'
+        # Personalizar el subject con el nombre y el teléfono
+        subject = f"Tienes una solicitud de consulta de {sender_name} ({sender_phone})"
 
-        # Create a Message object for the email
-        msg = Message(subject, sender='consultasapp2023@gmail.com', recipients=[recipient])
-        msg.body = complete_message  # Use the complete message
+        # Personalizar el message con fecha y hora
+        current_datetime = datetime.now().strftime("%d-%m-%Y %H:%M")
+        message = f"Tienes una solicitud de reserva el día de hoy, {current_datetime}.\n\n{message}"
+
+        # Combinar el mensaje completo con texto adicional
+        complete_message = f"{message}\n\n{additional_text}"
+
+        # Crear un objeto Message para el correo con el "No responder"
+        sender_address = "consultasapp2023@gmail.com"
+        sender_full = f"No responder <{sender_address}>"
+        msg = Message(subject, sender=sender_full, recipients=[recipient])
+        msg.body = complete_message
 
         try:
-            # Send the email
+            # Enviar el correo
             mail.send(msg)
             return jsonify({'message': 'Email sent successfully'}), 200
         except Exception as e:
